9afa837f85358dde8e33b6b8469d9d77f31164b4
Miguel Panelo <miguelrpanelo@gmail.com>
2020-04-24 22:23:31 +0200

    Convert to kotlin buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext.kotlin_version = "1.3.72"
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:4.1.0-alpha06"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:4.1.0-alpha06")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -21,6 +20,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+val clean by tasks.creating(Delete::class) {
+    delete(rootProject.buildDir)
 }
