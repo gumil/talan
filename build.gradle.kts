05328cd456d557c6840dcff48e4191db6e21e63e
Miguel Panelo <miguelrpanelo@gmail.com>
2020-05-13 22:18:36 +0200

    Refactor to make use of buildSrc constants

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.0-alpha09")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+        classpath(Plugins.android)
+        classpath(Plugins.kotlin)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
