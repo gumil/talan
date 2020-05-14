48eb4046e62bd19a8c0650f1dd4b174a6514d3fc
Miguel Panelo <miguelrpanelo@gmail.com>
2020-05-14 16:32:32 +0200

    Change url

--- backend/src/main/kotlin/dev/gumil/talan/backend/Root.kt
+++ backend/src/main/kotlin/dev/gumil/talan/backend/Root.kt
@@ -6,5 +6,5 @@ import io.kotless.dsl.lang.http.Get
 private val androidWeeklyApi = Factory.androidWeeklyApi()
 private val moshi = Factory.moshi()
 
-@Get("/", MimeType.JSON)
-fun root(): String = Factory.androidWeeklyJson(androidWeeklyApi, moshi)
+@Get("/androidweekly", MimeType.JSON)
+fun androidWeekly(): String = Factory.androidWeeklyJson(androidWeeklyApi, moshi)
