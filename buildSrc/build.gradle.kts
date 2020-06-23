f7219aa88d0d5ef07ea76486508a35abd990aef6
Miguel Panelo <miguelrpanelo@gmail.com>
2020-06-23 20:36:29 +0200

    Update node plugin to v2.2.4

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -18,6 +18,6 @@ repositories {
 dependencies {
     implementation("com.android.tools.build:gradle:4.2.0-alpha02")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
-    implementation("com.github.node-gradle:gradle-node-plugin:2.2.3")
+    implementation("com.github.node-gradle:gradle-node-plugin:2.2.4")
     implementation(gradleApi())
 }
