ee5c64c4a2db790fdcc65c75a6fd6def2199489b
Miguel Panelo <miguelrpanelo@gmail.com>
2020-06-24 21:26:36 +0200

    Add coroutines dependency

--- buildSrc/src/main/kotlin/deps.kt
+++ buildSrc/src/main/kotlin/deps.kt
@@ -2,6 +2,7 @@ internal object Versions {
     const val kotlin = "1.3.72"
     const val jacoco = "0.8.5"
     const val mockito = "2.2.0"
+    const val coroutines = "1.3.7"
 }
 
 internal object Plugins {
@@ -10,4 +11,11 @@ internal object Plugins {
 
 internal object Common {
     const val mockito = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockito}"
+}
+
+internal object Coroutines {
+    const val common = "org.jetbrains.kotlinx:kotlinx-coroutines-core-common:${Versions.coroutines}"
+    const val jvm = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
+    const val js = "org.jetbrains.kotlinx:kotlinx-coroutines-core-js:${Versions.coroutines}"
+    const val native = "org.jetbrains.kotlinx:kotlinx-coroutines-core-native:${Versions.coroutines}"
 }
