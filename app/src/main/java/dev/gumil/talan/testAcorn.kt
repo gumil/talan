0c869cf60771cfa807484ef4475804db4e8ad4f5
Miguel Panelo <miguelrpanelo@gmail.com>
2020-07-04 16:40:14 +0200

    Test ui list

--- app/src/main/java/dev/gumil/talan/testAcorn.kt
+++ app/src/main/java/dev/gumil/talan/testAcorn.kt
@@ -2,44 +2,62 @@ package dev.gumil.talan
 
 import android.util.Log
 import androidx.compose.Composable
+import androidx.ui.core.Modifier
 import androidx.ui.foundation.Text
-import androidx.ui.material.Button
-import androidx.ui.material.Scaffold
+import androidx.ui.foundation.lazy.LazyColumnItems
+import androidx.ui.foundation.shape.corner.RoundedCornerShape
+import androidx.ui.graphics.Color
+import androidx.ui.layout.fillMaxWidth
+import androidx.ui.layout.padding
+import androidx.ui.material.Card
+import androidx.ui.material.ListItem
+import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
 import com.nhaarman.acorn.presentation.Container
 import com.nhaarman.acorn.presentation.Scene
 import dev.gumil.talan.acorn.ComposeContainer
 import dev.gumil.talan.acorn.ComposeContainerFactory
 
-internal class Home: ComposeContainer {
+internal class Home : ComposeContainer {
     var onClick: () -> Unit = {
         Log.d("tantrums", "click should not be seen")
     }
 
+    val items = (0..20).map {
+        "Hello $it" to ("world ${20 - it}")
+    }
+
+    @Preview
     @Composable
     override fun render() {
-        Scaffold(
-            bodyContent = {
-                Text(text = "Hello Scene ONE!")
-                Button(onClick = {
-                    onClick()
-                }) {
-                    Text(text = "Click to go to next scene!")
-                }
+        LazyColumnItems(items) { hello ->
+            Card(
+                shape = RoundedCornerShape(4.dp), color = Color.White,
+                modifier = Modifier.fillMaxWidth() + Modifier.padding(8.dp)
+            ) {
+                ListItem(
+                    text = {
+                        Text(text = hello.first)
+                    },
+                    secondaryText = {
+                        Text(text = hello.second)
+                    }
+                )
             }
-        )
+        }
     }
 }
 
-internal class OtherScreen: ComposeContainer {
+internal class OtherScreen : ComposeContainer {
     @Composable
     override fun render() {
         Text(text = "Hello Scene OtherScreen!")
     }
 }
 
-internal class ComposeContainerFactoryImpl: ComposeContainerFactory {
+internal class ComposeContainerFactoryImpl : ComposeContainerFactory {
     override fun composeContainerFor(scene: Scene<out Container>): ComposeContainer {
-        return when(scene.key.value) {
+        return when (scene.key.value) {
             TestSceneOne::class.java.name -> Home()
             TestSceneTwo::class.java.name -> OtherScreen()
             else -> error("Not supported")
@@ -49,7 +67,7 @@ internal class ComposeContainerFactoryImpl: ComposeContainerFactory {
 
 internal class TestSceneOne(
     private val listener: Events
-): Scene<Home> {
+) : Scene<Home> {
 
     override fun attach(v: Home) {
         v.onClick = listener::onClickButton
@@ -60,4 +78,4 @@ internal class TestSceneOne(
     }
 }
 
-internal class TestSceneTwo: Scene<ComposeContainer>
+internal class TestSceneTwo : Scene<ComposeContainer>
