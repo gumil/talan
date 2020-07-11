3659cc6f48e165a6b5db06a7a63d53d25d89d035
Miguel Panelo <miguelrpanelo@gmail.com>
2020-07-11 12:24:03 +0200

    Add packForXcode task

--- buildSrc/src/main/kotlin/plugin/MultiplatformConfigurationPlugin.kt
+++ buildSrc/src/main/kotlin/plugin/MultiplatformConfigurationPlugin.kt
@@ -2,9 +2,13 @@ package plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Sync
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.register
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import java.io.File
 
 class MultiplatformConfigurationPlugin : Plugin<Project> {
     override fun apply(project: Project) {
@@ -31,12 +35,46 @@ class MultiplatformConfigurationPlugin : Plugin<Project> {
                 }
             }
 
-            val onPhone = System.getenv("SDK_NAME")?.startsWith("iphoneos") ?: false
-            if (onPhone) {
-                iosArm64("ios")
-            } else {
-                iosX64("ios")
+            val iOSTargetConfiguration: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
+                if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
+                    ::iosArm64
+                else
+                    ::iosX64
+
+            val iOSTarget = iOSTargetConfiguration("ios") {
+                binaries {
+                    framework {
+                        baseName = "SharedClient"
+                    }
+                }
+            }
+            project.configurePackForXcode(iOSTarget)
+        }
+    }
+
+    private fun Project.configurePackForXcode(target: KotlinNativeTarget) {
+        val packForXcode = "packForXcode"
+        tasks.register<Sync>(packForXcode) {
+            group = "build"
+
+            //selecting the right configuration for the iOS framework depending on the Xcode environment variables
+            val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
+            val framework = target.binaries.getFramework(mode)
+
+            inputs.property("mode", mode)
+            dependsOn(framework.linkTask)
+
+            val targetDir = File(buildDir, "xcode-frameworks")
+            from({ framework.outputDirectory })
+            into(targetDir)
+
+            doLast {
+                val gradlew = File(targetDir, "gradlew")
+                gradlew.writeText("#!/bin/bash\nexport 'JAVA_HOME=${System.getProperty("java.home")}'\ncd '${rootProject.rootDir}'\n./gradlew \$@\n")
+                gradlew.setExecutable(true)
             }
         }
+
+        tasks.getByName("build").dependsOn(packForXcode)
     }
 }
