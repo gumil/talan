5cee251a86eb6026a8d296f0f25054cf56ebd4fb
Miguel Panelo <miguelrpanelo@gmail.com>
2020-11-19 23:11:30 +0100

    Remove kotlin reflect

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,10 +16,8 @@ repositories {
 }
 
 dependencies {
-    val kotlinVersion = "1.4.10"
     implementation("com.android.tools.build:gradle:4.2.0-alpha15")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
     implementation("com.squareup.sqldelight:gradle-plugin:1.4.4")
     implementation("com.github.ben-manes:gradle-versions-plugin:0.36.0")
     implementation(gradleApi())
